bae6f578c473c669348cb54e095bbe48fabf2186
HB6016\user <jangdduk3109@daum.net>
2016-11-18 12:48:47 +0900

    maven적용 mvc2 스프링 따라하기 셋팅

---
+++ ex04/src/main/java/com/mvc2/controller/InterController.java
@@ -0,0 +1,10 @@
+package com.mvc2.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface InterController {
+
+	
+	String execute(HttpServletRequest req, HttpServletResponse resp);
+}
---
+++ ex04/src/main/java/com/mvc2/controller/ListImp.java
@@ -0,0 +1,15 @@
+package com.mvc2.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListImp implements InterController {
+
+	public String execute(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+		
+		return "/guest/list";
+	}
+
+}
